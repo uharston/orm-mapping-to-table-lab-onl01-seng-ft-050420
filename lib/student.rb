f54cbcda403b2c716920205efa9c35eeeecd181c
Uriah Harston <uharston@gmail.com>
2020-06-02 20:48:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,11 @@ class Student
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")
-    end 
+  end 
   
+  def self.create 
+    Student.new 
+    
+  end 
   
 end

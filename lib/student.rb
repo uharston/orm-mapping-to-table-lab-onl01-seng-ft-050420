0c72ba36489f75263e3411750fe6d0d9c7a3048d
Uriah Harston <uharston@gmail.com>
2020-06-02 20:33:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,22 @@
 class Student
   
-  attr_accessor :name, :grade, :id 
+  attr_accessor :name, :grade
+  attr_reader :id 
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  def initialize(name, grade, id=nil)
+    @name = name 
+    @grade = grade
+    @id = id 
+  end 
+  
+  def self.create_table
+    sql <<-SQL 
+    CREATE TABLE student (
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    grade INTEGER)
+    
+    
+  end 
   
 end

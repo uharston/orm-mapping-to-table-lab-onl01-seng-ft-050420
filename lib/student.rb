fde495591e79388fc99b6a71f51b4cb2bfde488f
Uriah Harston <uharston@gmail.com>
2020-06-02 20:44:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?)
       SQL
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql, self.name, self,grade)
       @id = 
   
   

207d3829677d785356d94c11d5de0593348e7ded
Uriah Harston <uharston@gmail.com>
2020-06-02 20:50:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,14 +27,14 @@ class Student
       DB[:conn].execute(sql)
   end 
   
-  def save 
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-      SQL
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")
-  end 
+  # def save 
+  #   sql = <<-SQL
+  #     INSERT INTO students (name, grade)
+  #     VALUES (?, ?)
+  #     SQL
+  #     DB[:conn].execute(sql, self.name, self.grade)
+  #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")
+  # end 
     def save
     sql = <<-SQL
       INSERT INTO students (name, grade)

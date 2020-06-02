1deb1a74dc9f9195fa37f4e7fef6e2904796909f
Uriah Harston <uharston@gmail.com>
2020-06-02 20:40:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,5 +20,12 @@ class Student
     DB[:conn].execute(sql) 
   end
   
+  def self.drop_table 
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+      DB[:conn].execute(sql)
+  end 
+  
   
 end

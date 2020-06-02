aad8539980806f834acbcb2aba1e85747b3288aa
Uriah Harston <uharston@gmail.com>
2020-06-02 20:38:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,23 +11,14 @@ class Student
   
     def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        album TEXT
+        grade INTEGER
         )
         SQL
     DB[:conn].execute(sql) 
   end
   
-  def self.create_table
-    sql = <<-SQL 
-    CREATE TABLE IF NOT EXIST students (
-    id INTEGER PRIMARY KEY, 
-    name TEXT, 
-    grade INTEGER)
-    SQL 
-    DB[:conn].execute(sql)
-  end 
   
 end

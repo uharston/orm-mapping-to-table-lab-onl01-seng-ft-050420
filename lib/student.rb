928bad349b5336fcfe8a28640a36db37a6f114ed
Uriah Harston <uharston@gmail.com>
2020-06-02 20:52:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,14 +35,13 @@ class Student
   #     DB[:conn].execute(sql, self.name, self.grade)
   #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")
   # end 
-    def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-        VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
+def save
+  sql = <<-SQL
+  insert into students (name,grade) values(?,?);
+  SQL
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
+end
   def self.create(name, grade)
     song = Song.new(name, grade)
     song.save
